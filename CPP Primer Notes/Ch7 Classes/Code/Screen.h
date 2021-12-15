87b304b7c3b970c74c4d14f06dfcc7e3fe8c111d
Guanzhong Pan <gpan20020205@gmail.com>
2021-12-15 22:34:33 +0000

    Update code

--- CPP Primer Notes/Ch7 Classes/Code/Screen.h
+++ CPP Primer Notes/Ch7 Classes/Code/Screen.h
@@ -15,12 +15,19 @@ public:
     inline char get(pos, pos) const;
     inline Screen &move(pos, pos);
     inline pos getCallCount() const;
+    Screen &set(char c);
+    Screen &display(std::ostream &);
+    const Screen &display(std::ostream &) const;
+
 
 private:
     pos cursor = 0;
     pos width = 0, height = 0;
     mutable pos call_count = 0;
     std::string contents;
+    void do_Display(std::ostream &os) const {
+        os << contents;
+    }
 };
 
 char Screen::get() const {
@@ -43,4 +50,19 @@ Screen &Screen::move(pos r, pos c) {
 
 Screen::pos Screen::getCallCount() const {
     return this->call_count;
+}
+
+inline Screen &Screen::set(char c) {
+    this->contents[cursor] = c;
+    return *this;
+}
+
+inline Screen &Screen::display(std::ostream &os) {
+    do_Display(os);
+    return *this;
+}
+
+inline const Screen &Screen::display(std::ostream &os) const {
+    do_Display(os);
+    return *this;
 }
--- CPP Primer Notes/Ch7 Classes/Code/ScreenMain.cpp
+++ CPP Primer Notes/Ch7 Classes/Code/ScreenMain.cpp
@@ -3,7 +3,14 @@
 int main(int argc, char* argv[]) {
     Screen s = Screen(30, 30);
     s.move(20,20);
+    s.set('c');
     std::cout << s.get() << std::endl;
     std::cout << s.getCallCount() << std::endl;
+
+    Screen myScreen(5, 5, 'X');
+    myScreen.move(4,0).set('#').display(cout);
+    cout << "\n";
+    myScreen.display(cout);
+    cout << "\n";
     return 0;
 }
