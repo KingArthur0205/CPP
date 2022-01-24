6ccf3500ca7ca476eff8553f840fb443d9d8b3ee
Guanzhong Pan <gpan20020205@gmail.com>
2022-01-24 12:56:14 +0000

    Add Code to 11_7.cpp

--- CPP Primer Notes/Ch11 Associative Container/Code/11_7.cpp
+++ CPP Primer Notes/Ch11 Associative Container/Code/11_7.cpp
@@ -4,16 +4,19 @@
 #include <set>
 #include <vector>
 
+void addChildren(std::map<std::string, std::vector<std::string>> &familyTree, const std::string &lName, const std::string &fName) {
+    familyTree[lName].push_back(fName);
+}
+
 int main(int argc, char* argv[]) {
     std::map<std::string, std::vector<std::string>> familyTree;
     std::string familyName;
-    if(std::cin >> familyName) {
-        std::vector<std::string> tempVec;
+    while(std::cin >> familyName) {
         std::cin.clear();
         std::string bufferStr;
         while(std::cin >> bufferStr)
-            tempVec.push_back(bufferStr);
-        familyTree[familyName] = tempVec;
+            addChildren(familyTree, familyName, bufferStr);
+        std::cin.clear();
     }
 
     for(auto &elem : familyTree) {
